0da60a85806827c0e69b035f2cf12228f7efda7a
RAHUL RUDRA <rahulrudra665@gmail.com>
2025-11-17 11:19:21 +0600

    changesmade"

--- New Batch-06/Conceptual Session/2.Types/dist/GenericWithFunction.js
+++ New Batch-06/Conceptual Session/2.Types/dist/GenericWithFunction.js
@@ -3,5 +3,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const greet = (a, b) => {
     return a + b;
 };
-console.log(greet(23.6, 9));
+// console.log(greet(23.6, 9));
+// ------------------------------
+function getFirstIndex(array) {
+    return array[0];
+}
+let number = getFirstIndex([45, 78]);
+let string = getFirstIndex(["Rahul"]);
+console.log(string);
+console.log(number);
+// ------------------------------
+function length1(value) {
+    return value.length;
+}
+console.log(length1("efe"));
+// -----------
+function add(number1) {
+    return number1.reduce((a, b) => a + b, 0);
+}
+console.log(add([1, 3, 4, 5]));
 //# sourceMappingURL=GenericWithFunction.js.map
