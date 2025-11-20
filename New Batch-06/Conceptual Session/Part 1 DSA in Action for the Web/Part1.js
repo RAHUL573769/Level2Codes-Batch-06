918184dc2760529d0b80586301de87fd0f43aa96
RAHUL RUDRA <rahulrudra665@gmail.com>
2025-11-20 21:36:08 +0600

    cahnges made"

---
+++ New Batch-06/Conceptual Session/Part 1 DSA in Action for the Web/Part1.js
@@ -0,0 +1,16 @@
+// Bigo(1)
+
+function bigOne(array) {
+  return array[1];
+}
+console.log(bigOne(["A", "B"]));
+
+//Big O(n)
+function BigO1(array1) {
+  for (i of array1) {
+    console.log(i);
+  }
+}
+console.log(bigOne(["A", "B"]));
+
+function zeroLogn() {}
