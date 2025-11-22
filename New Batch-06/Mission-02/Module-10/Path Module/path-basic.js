2793684ff4e7d5e21e281f5b619501cda152c10c
RAHUL RUDRA <rahulrudra665@gmail.com>
2025-11-22 10:22:37 +0600

    changes made"

---
+++ New Batch-06/Mission-02/Module-10/Path Module/path-basic.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+console.log("Current Path Info");
+console.log("File Name", __filename);
+console.log("Dir Name", __dirname);
+console.log("\n" + "-".repeat(50), +"\n");
+
+const filePath = "/rahul/documents/next-level.pdf";
+console.log("Anayzing File Path", filePath);
