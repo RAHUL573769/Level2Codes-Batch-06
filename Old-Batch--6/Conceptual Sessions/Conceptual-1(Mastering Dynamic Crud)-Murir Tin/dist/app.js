b043756d73c4a4cfb044d483d5fbc5712582d95a
RAHUL RUDRA <rahulrudra665@gmail.com>
2025-11-26 10:24:45 +0600

    murit tin started 1"

--- Old-Batch--6/Conceptual Sessions/Conceptual-1(Mastering Dynamic Crud)-Murir Tin/dist/app.js
+++ Old-Batch--6/Conceptual Sessions/Conceptual-1(Mastering Dynamic Crud)-Murir Tin/dist/app.js
@@ -5,8 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.get("/", (_req, res) => {
-    res.send("Hi From JS");
+const userRouter = express_1.default.Router();
+userRouter.get("/", (_req, res) => {
+    res.status(200).json({
+        message: "Welcome To Murir Tin",
+        status: "success",
+    });
 });
+app.use("/api/v1/users", userRouter);
+// app.get("/", (_req: Request, res: Response) => {
+//   res.status(200).json({
+//     message: "Welcome To Murir Tin",
+//     status: "success",
+//   });
+// });
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- Old-Batch--6/Conceptual Sessions/Conceptual-1(Mastering Dynamic Crud)-Murir Tin/src/app.ts
+++ Old-Batch--6/Conceptual Sessions/Conceptual-1(Mastering Dynamic Crud)-Murir Tin/src/app.ts
@@ -1,8 +1,20 @@
-import express, { Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
 
-const app = express();
+const app: Application = express();
 
-app.get("/", (_req: Request, res: Response) => {
-  res.send("Hi From JS");
+const userRouter = express.Router();
+userRouter.get("/", (_req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Welcome To Murir Tin",
+    status: "success",
+  });
 });
+
+app.use("/api/v1/users", userRouter);
+// app.get("/", (_req: Request, res: Response) => {
+//   res.status(200).json({
+//     message: "Welcome To Murir Tin",
+//     status: "success",
+//   });
+// });
 export default app;
