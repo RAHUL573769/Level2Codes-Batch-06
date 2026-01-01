0dfaef9f0f34a862bed99d864f30c1b34187da4f
RAHUL RUDRA <rahulrudra665@gmail.com>
2026-01-01 08:44:35 +0600

    commit changes

---
+++ New Batch-06/Conceptual Session/LabEquipmetTracker/src/modules/user/user.controler.ts
@@ -0,0 +1,13 @@
+import { RequestHandler } from "express"
+import { prisma } from "../../lib/prisma"
+
+const register: RequestHandler = async (req, res) => {
+    console.log("Register Working")
+    const payload = req.body
+    const user = await prisma.user.create({
+        data: payload
+    })
+    res.send({ message: "Registed User Sucessfully", data: user })
+}
+
+export const userController = { register }
